2b13fc93455304644cfb270ebc7f8c1e7883843b
Pedro Elias <pedro-elias1997@hotmail.com>
2019-11-11 14:53:28 -0300

    Commit #9

--- code/Customer.java
+++ code/Customer.java
@@ -37,7 +37,7 @@ class Customer {
      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints) +
              " frequent renter points";
      return result;
-
+  }
   private double getTotalCharge() {
       double result = 0;
       Enumeration rentals = _rentals.elements();
@@ -56,5 +56,23 @@ class Customer {
           result += each.getFrequentRenterPoints();
       }
       return result;
+    }
+
+  public String htmlStatement() {
+     Enumeration rentals = _rentals.elements();
+     String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+     while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        // show figures for each rental
+        result += each.getMovie().getTitle()+ ": " +
+                  String.valueOf(each.getCharge()) + "<BR>\n";
+      }
+
+     // add footer lines
+     result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+     result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+     return result;
     }
 }
